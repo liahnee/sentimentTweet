267897513fbbd637c56e21e567004726f3b600dc
Catherine Liahn Yang <liahnee@gmail.com>
2020-06-29 22:07:47 -0700

    changed from .then to using data from fetch to render cards. tweets are being saved on redux for uses on different tabs

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,4 @@
-// no empty cards. render cards only if prop.tweets are available.
+// no empty cards. render cards only if prop.tweets are available. 
 
 import React, { useState } from 'react';
 import '../assets/stylesheets/Home.css';
@@ -10,6 +10,7 @@ import { connect } from 'react-redux';
 
 import FullDropdown from '../components/Dropdown';
 
+
 const url = 'http://localhost:3000';
 
 const Home = (props) => {
@@ -20,65 +21,49 @@ const Home = (props) => {
 
 	// const check = (num) => {
 	//     return allCards[num]? allCards[num] : <Image src={bcard} />
-	// }
+    // }
+    
+    const [ loading, setLoading] = useState(false);
 
-	const [ loading, setLoading ] = useState(false);
 
-	const renderCards = () => {
-		const cardsDiv = document.getElementById('cards-grid-container');
-		console.log('rendering cards... props.selectedCeleb', props.selectedCeleb);
-		console.log('rendering cards... props.tweets', props.tweets);
+	const renderCards = (tweets) => {
+        const cardsDiv = document.getElementById("cards-grid-container");
 
-		if (props.tweets && props.tweets.length > 0) {
-			props.tweets.forEach((tweet) => {
-				cardsDiv.appendChild(<img src={bcard} />);
-			});
-		} else {
-			return null;
-		}
-	};
 
+        tweets.forEach((tweet) => {
+            let img = document.createElement("img")
+            img.src = bcard;
+            cardsDiv.appendChild(img);
+        });
+    };
+    
 	const handleChange = (e, item) => {
 		const id = item.value;
 		// console.log(id)
-		// props.selectCeleb(id);
+		props.selectCeleb(id);
 		setLoading(true);
-        // fetchSelect(id);
-        
-
-        props.addTweets(id).then(() => {setLoading(false); renderCards()})
+		fetchSelect(id);
 	};
 
 	const fetchSelect = async (payload) => {
-		fetch(url + `/celebs/${payload}`).then((resp) => resp.json()).then((data) => {
-			console.log(data);
-			setLoading(false);
-			props.addTweets(data).then(() => {
-				renderCards();
-			});
-		});
+		fetch(url + `/celebs/${payload}`)
+			.then((resp) => resp.json())
+			.then((data) => {
+				console.log(data);
+				setLoading(false);
+                props.addTweets(data);
+                renderCards(data);
+            })
+            .catch(err => console.log(err))
 	};
+
 
 	return (
 		<div className="home">
-			<div id="cards-grid-container">{/* create flip cards and ref for each */}</div>
-			{loading ? (
-				<div
-					style={{
-						color: 'white',
-						fontSize: '3rem',
-						position: 'absolute',
-						margin: '0 auto 0 auto',
-						top: '0',
-						bottom: '0',
-						left: '0',
-						right: '0',
-						zIndex: '5'
-					}}
-				>
-					Loading...{' '}
-				</div>
-			) : null}
+			<div id="cards-grid-container">
+                {/* create flip cards and ref for each */}
+            </div>
+            {loading ? <div style={{color:"white", fontSize:"3rem", position: "absolute", margin: "0 auto 0 auto", top: "0", bottom: "0", left: "0", right: "0", zIndex: "5"}}>Loading... </div> : null}
 			<FullDropdown handleChange={handleChange} />
 		</div>
 	);
@@ -95,23 +80,15 @@ const sToP = (state) => {
 
 const dToP = (dispatch) => ({
 	addAllCelebs: (data) => dispatch({ type: 'ADD_CELEBS', payload: data }),
-	selectCeleb: (data) => dispatch({ type: 'SELECT_CELEB', payload: data }),
-	addTweets: (data) => {
-		return fetch(url + `/celebs/${data}`).then((resp) => resp.json()).then((json) =>
-			dispatch({
-				type: 'ADD_TWEETS',
-				payload: json
-			})
-		);
-	}
-	// new Promise((resolve, reject) => {
-	//     dispatch({
-	//       type: 'ADD_TWEETS',
-	//       payload: data
-	//     });
-	//     resolve()
-	// }),
-	// (data) => dispatch({ type: , })
+    selectCeleb: (data) => dispatch({ type: 'SELECT_CELEB', payload: data }),
+    addTweets: data => new Promise((resolve, reject) => {
+        dispatch({
+          type: 'ADD_TWEETS',
+          payload: data 
+        });
+        resolve()
+    }),
+    // (data) => dispatch({ type: , })
 });
 
 export default connect(sToP, dToP)(Home);
