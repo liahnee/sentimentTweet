c2c4f684663895737d1510afc4c29973f92e444a
Catherine Liahn Yang <liahnee@gmail.com>
2020-07-02 16:09:19 -0700

    card css

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,4 @@
-// no empty cards. render cards only if prop.tweets are available. 
+// no empty cards. render cards only if prop.tweets are available.
 
 import React, { useState, useEffect } from 'react';
 import '../assets/stylesheets/Home.css';
@@ -9,12 +9,10 @@ import { connect } from 'react-redux';
 
 import FullDropdown from '../components/Dropdown';
 
-
 const url = 'http://localhost:3000';
 
 const Home = (props) => {
-
-    const [tweets, setTweets] = useState([{}, {}, {}, {}, {}, {}, {}, {}, {}]);
+	const [ tweets, setTweets ] = useState([ {}, {}, {}, {}, {}, {}, {}, {}, {} ]);
 	// const allCards = props.tweets.map( tweet => {
 	//     console.log('searchHOme')
 	//     return <CardFlip tweet={tweet}/>
@@ -22,10 +20,10 @@ const Home = (props) => {
 
 	// const check = (num) => {
 	//     return allCards[num]? allCards[num] : <Image src={bcard} />
-    // }
-    
-    const [ loading, setLoading] = useState(false);
-	useEffect(()=>{}, tweets)
+	// }
+
+	const [ loading, setLoading ] = useState(false);
+	useEffect(() => {}, tweets);
 
 	const handleChange = (e, item) => {
 		const id = item.value;
@@ -41,18 +39,40 @@ const Home = (props) => {
 			.then((data) => {
 				console.log(data);
 				setLoading(false);
-                props.addTweets(data)
-                setTweets(data);
-            })
-            .catch(err => console.log(err))
+				props.addTweets(data);
+				setTweets(data);
+			})
+			.catch((err) => console.log(err));
 	};
 
 	return (
 		<div className="home">
 			<div id="cards-grid-container">
-			{tweets[0].tweet ? tweets.map(tweet => { return <CardFlip tweet={tweet} /> }) : null}
-            </div>
-            {loading ? <div style={{color:"white", fontSize:"3rem", position: "absolute", margin: "0 auto 0 auto", top: "0", bottom: "0", left: "0", right: "0", zIndex: "5"}}>Loading... </div> : null}
+				<div id="cards-grid">
+					{tweets[0].tweet ? (
+						tweets.map((tweet) => {
+							return <CardFlip tweet={tweet} />;
+						})
+					) : null}
+				</div>
+			</div>
+			{loading ? (
+				<div
+					style={{
+						color: 'white',
+						fontSize: '3rem',
+						position: 'absolute',
+						margin: '0 auto 0 auto',
+						top: '0',
+						bottom: '0',
+						left: '0',
+						right: '0',
+						zIndex: '5'
+					}}
+				>
+					Loading...{' '}
+				</div>
+			) : null}
 			<FullDropdown handleChange={handleChange} />
 		</div>
 	);
@@ -67,24 +87,25 @@ const sToP = (state) => {
 	};
 };
 
-const tweetsDispatch = (data, dispatch) => new Promise((resolve, reject) => {
-    // do anything here
-    dispatch({ type: 'ADD_TWEETS', payload: data });
-    resolve();
-  });
+const tweetsDispatch = (data, dispatch) =>
+	new Promise((resolve, reject) => {
+		// do anything here
+		dispatch({ type: 'ADD_TWEETS', payload: data });
+		resolve();
+	});
 
 const dToP = (dispatch) => ({
 	addAllCelebs: (data) => dispatch({ type: 'ADD_CELEBS', payload: data }),
-    selectCeleb: (data) => dispatch({ type: 'SELECT_CELEB', payload: data }),
-    addTweets: data => tweetsDispatch(data, dispatch)
-    // new Promise((resolve, reject) => {
-    //     dispatch({
-    //       type: 'ADD_TWEETS',
-    //       payload: data 
-    //     });
-    //     resolve()
-    // }),
-    // (data) => dispatch({ type: , })
+	selectCeleb: (data) => dispatch({ type: 'SELECT_CELEB', payload: data }),
+	addTweets: (data) => tweetsDispatch(data, dispatch)
+	// new Promise((resolve, reject) => {
+	//     dispatch({
+	//       type: 'ADD_TWEETS',
+	//       payload: data
+	//     });
+	//     resolve()
+	// }),
+	// (data) => dispatch({ type: , })
 });
 
 export default connect(sToP, dToP)(Home);
