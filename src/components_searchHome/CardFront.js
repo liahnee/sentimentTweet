35845eadeaa94d93fc74482f325652d74c9644c6
Catherine Liahn Yang <liahnee@gmail.com>
2020-07-22 18:13:50 -0700

    card front hue fixed from state to function

--- src/components_searchHome/CardFront.js
+++ src/components_searchHome/CardFront.js
@@ -3,17 +3,21 @@ import { Card } from 'semantic-ui-react';
 import moment from 'moment';
 
 const CardFront = (props) => {
-	const [ hue ] = useState(props.sent >= 0 ? 'yellow' : 'blue');
 
 	const icon = () => {
 		if (props.sent > 0) {
+			
 			return 'smile outline';
 		} else if (props.sent < 0) {
 			return 'frown outline';
 		} else {
 			return 'selected radio';
 		}
-    };
+	};
+	
+	const hue = () => {
+		return props.sent < 0? 'blue' : 'yellow'
+	}
     const dateFormat = 'DDD MMM DD HH:MM:SS Z YYYY';
     const weekdayNum = moment(props.date, dateFormat).weekday();
 
@@ -26,7 +30,7 @@ const CardFront = (props) => {
                     {console.log("testing moment", moment().isoWeekday(true, weekdayNum))}
 					{moment(props.date, dateFormat).format('LT')}
 					<span className="right floated">
-						<i className={`icon-left large ${icon()} ${hue} icon`} />
+						<i className={`icon-left large ${icon()} ${hue()} icon`} />
 					</span>
 				</div>
 				<div className="description">
